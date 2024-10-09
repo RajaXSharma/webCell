c40899a0d27fc85b66cdf8648722a6b8ffd6ab10
RajaXSharma <rajasharma844891@gmail.com>
2024-10-09 23:12:49 +0530

    fix

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -17,9 +17,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <button>{count}</button>
       <div className="card">
         <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-          increase {count}
+          increase
         </button>
         <button onClick={() => setCount((prevCount) => prevCount - 1)}>
           decrease
